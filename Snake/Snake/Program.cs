2e03a19e9c70d9fb3dc1613fdb20b72405eed1c3
Adfansy <elena_gomalach@mail.ru>
2019-03-08 15:33:43 +0300

    Нифига не понятно..(

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,7 +11,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.SetBufferSize( 120, 30 );
+            /*Console.SetBufferSize( 120, 30 );
 
             // отрисовка рамки           
             HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
@@ -50,7 +50,30 @@ namespace Snake
                     ConsoleKeyInfo key = Console.ReadKey();
                     Snake.HandleKey( key.Key );
                 }                
-            }           
-        }       
+            }  */
+            VerticalLine vl = new VerticalLine(0, 10, 5, '%');
+            Draw(vl);
+
+            Point p = new Point(4, 5, '*');
+            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            Draw(fSnake);
+            Snake snake = (Snake)fSnake;
+
+            HorizontalLine hl = new HorizontalLine(0, 5, 6, '&');
+
+            List<Figure> figures = new List<Figure>();
+            figures.Add(fSnake);
+            figures.Add(vl);
+            figures.Add(hl);
+
+            foreach (var f in figures)
+            {
+                f.Draw();
+            }
+        }
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
+        }
     }
 }
