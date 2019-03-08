6a762a53c16628132953d665cf2c007893b66db6
Adfansy <elena_gomalach@mail.ru>
2019-03-08 14:49:29 +0300

    Создание класса Еда. Увеличение Змейки после еды.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -28,18 +28,29 @@ namespace Snake
             Snake Snake = new Snake( p, 4, Direction.RIGHT );
             Snake.Draw();
 
+            FoodCreator foodCreator = new FoodCreator( 78, 24, '$' );
+            Point food = foodCreator.CreatFood();
+            food.Draw();
+
             while(true)
             {
+                if(Snake.Eat( food ) )
+                {
+                    food = foodCreator.CreatFood();
+                    food.Draw();
+                }
+                else
+                {
+                    Snake.Move();
+                }
+                Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     Snake.HandleKey( key.Key );
-                }
-                Thread.Sleep(100);
-                Snake.Move();
-            }
-            Console.ReadLine();
-        }
-       
+                }                
+            }           
+        }       
     }
 }
